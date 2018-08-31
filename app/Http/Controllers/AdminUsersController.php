2d10cd382170b28a989358119e94041a543647c0
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-08-31 09:55:32 -1000

    users with uploading feature working

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\UsersRequest;
+use App\Photo;
 use App\User;
 use App\Role;
 use Illuminate\Http\Request;
@@ -50,10 +51,28 @@ class AdminUsersController extends Controller
     public function store(UsersRequest $request)
     {
         //
+        $input = $request->all();
 
-        User::create($request->all());
+        if($file = $request->file('photo_id')){
 
-        return redirect('/admin/users');
+          $name = time() . $file->getClientOriginalName();
+
+          $file->move('images', $name);
+
+          $photo = Photo::create(['file'=>$name]);
+
+          $input['photo_id'] = $photo->id;
+
+        }
+
+        $input['password'] = bcrypt($request->password);
+
+        User::create($input);
+
+
+
+
+        // return redirect('/admin/users');
 
         // return $request->all();
     }
