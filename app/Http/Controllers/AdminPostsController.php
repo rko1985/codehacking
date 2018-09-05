e26deb092f66c7a5e4cdc2f0ece8d8971efb114a
rko1985 <35703264+rko1985@users.noreply.github.com>
2018-09-05 07:52:05 -1000

    able to create categories lesson 245

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -95,6 +95,11 @@ class AdminPostsController extends Controller
     public function edit($id)
     {
         //
+        $post = Post::findOrFail($id);
+
+        $categories = Category::lists('name', 'id')->all();
+
+        return view('admin.posts.edit', compact('post','categories'));
     }
 
     /**
@@ -107,6 +112,24 @@ class AdminPostsController extends Controller
     public function update(Request $request, $id)
     {
         //
+        $input = $request->all();
+
+        if($file = $request->file('photo_id')){
+
+          $name = time() . $file->getClientOriginalName();
+
+          $file->move('images', $name);
+
+          $photo = Photo::create(['file'=>$name]);
+
+          $input['photo_id'] = $photo->id;
+
+        }
+
+        Auth::user()->posts()->whereId($id)->first()->update($input);
+
+        return redirect('/admin/posts');
+
     }
 
     /**
@@ -118,5 +141,13 @@ class AdminPostsController extends Controller
     public function destroy($id)
     {
         //
+        $post = Post::findOrFail($id);
+
+        unlink(public_path() . $post->photo->file);
+
+        $post->delete();
+
+        return redirect('/admin/posts');
+
     }
 }
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -30,7 +30,9 @@ Route::group(['middleware'=>'admin'], function(){
 
   Route::resource('admin/users', 'AdminUsersController');
 
-  Route::resource('admin/posts', 'AdminPostsController');  
+  Route::resource('admin/posts', 'AdminPostsController');
+
+  Route::resource('admin/categories', 'AdminCategoriesController');  
 
 
 });
